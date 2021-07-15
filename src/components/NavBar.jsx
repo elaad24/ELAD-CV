f3546e9be95a9261f287121ed3b5d0045d03df41
unknown <elaad2408@gmail.com>
2021-07-15 21:12:24 +0300

    make the nav bar when in responsive to close after use

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 
+let collapse_div = document.querySelector("#navbarCollapse");
+
+let closeNavbar = (e) => {
+  if (e.target.className == "nav-link") {
+    collapse_div.classList.toggle("show");
+  }
+};
+
 const NavBar = () => {
   return (
     <nav class="navbar navbar-expand-lg navbar-dark fixed-top ">
       <div class="container-fluid">
         <a class="navbar-brand" href="#">
-          Fixed navbar
+          Navigation
         </a>
         <button
           class="navbar-toggler"
@@ -13,30 +21,35 @@ const NavBar = () => {
           data-bs-toggle="collapse"
           data-bs-target="#navbarCollapse"
           aria-controls="navbarCollapse"
-          aria-expanded="false"
+          aria-expanded="true"
           aria-label="Toggle navigation"
+          name="navbarBTN"
         >
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
           <ul class="navbar-nav mx-auto mb-2 mb-md-0" navigation>
             <li class="nav-item">
-              <a href="#about" className=" nav-link  ">
+              <a href="#about" className="nav-link" onClick={closeNavbar}>
                 ABOUT
               </a>
             </li>
             <li class="nav-item">
-              <a href="#educationAndProjects" className="nav-link ">
+              <a
+                href="#educationAndProjects"
+                className="nav-link"
+                onClick={closeNavbar}
+              >
                 EDUCATION & PROJECTS
               </a>
             </li>
             <li class="nav-item">
-              <a href="#army" className="nav-link ">
+              <a href="#army" className="nav-link" onClick={closeNavbar}>
                 ARMY
               </a>
             </li>
             <li class="nav-item">
-              <a href="#qualities" className="nav-link ">
+              <a href="#qualities" className="nav-link" onClick={closeNavbar}>
                 QUALITIES
               </a>
             </li>
